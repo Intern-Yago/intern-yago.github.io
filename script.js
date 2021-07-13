11b0645e94bbda3a9e5fa670bebb43a627148655
Syri-Yagowo <yagovictorbotafogo@gmail.com>
2021-07-13 18:00:23 -0300

    correção de erros JS

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ window.onload = addElemento;
 function addElemento(){
     var divAtual = document.getElementById('row')
     var cont = '<i class="fa fa-html5" aria-hidden="true"></i><i class="fa fa-css3" aria-hidden="true"></i><i class="fa fa-linux" aria-hidden="true"></i><i class="fa fa-windows" aria-hidden="true"></i><i class="fa fa-android" aria-hidden="true"></i><i class="fa fa-github" aria-hidden="true"></i><i class="fa fa-stack-overflow" aria-hidden="true"></i><i class="fa fa-code" aria-hidden="true"></i><i class="fa fa-terminal" aria-hidden="true"></i><i class="fa fa-battery-three-quarters" aria-hidden="true"></i><i class="fa fa-microchip" aria-hidden="true"></i><i class="fa fa-database" aria-hidden="true"></i>'
-    for(let i =0; i<2;i++){
+    for(let i =0; i<20;i++){
         var divNova = document.createElement("div")
         divNova.innerHTML = cont
         divAtual.appendChild(divNova)
     }
 }
+var largura = document.documentElement.clientWidth
+console.log(largura)
