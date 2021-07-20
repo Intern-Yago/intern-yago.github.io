9a0062f5c24da7d6f9375da857efdad09bb761c5
Syri-Yagowo <yagovictorbotafogo@gmail.com>
2021-07-20 02:35:36 -0300

    arrumando arrow/fundo | estilizando o main

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 window.onload = addElemento;
 function addElemento(){
+    var largura = document.body.clientWidth
+    console.log(largura)
+    console.log(largura/45) 
     var divAtual = document.getElementById('row')
-    var cont = '<i class="fa fa-html5" aria-hidden="true"></i><i class="fa fa-css3" aria-hidden="true"></i><i class="fa fa-linux" aria-hidden="true"></i><i class="fa fa-windows" aria-hidden="true"></i><i class="fa fa-android" aria-hidden="true"></i><i class="fa fa-github" aria-hidden="true"></i><i class="fa fa-stack-overflow" aria-hidden="true"></i><i class="fa fa-code" aria-hidden="true"></i><i class="fa fa-terminal" aria-hidden="true"></i><i class="fa fa-battery-three-quarters" aria-hidden="true"></i><i class="fa fa-microchip" aria-hidden="true"></i><i class="fa fa-database" aria-hidden="true"></i>'
-    for(let i =0; i<20;i++){
+    var cont = '<i class="fa fa-linux" aria-hidden="true"></i><i class="fa fa-windows" aria-hidden="true"></i><i class="fa fa-android" aria-hidden="true"></i><i class="fa fa-github" aria-hidden="true"></i><i class="fa fa-stack-overflow" aria-hidden="true"></i><i class="fa fa-code" aria-hidden="true"></i><i class="fa fa-terminal" aria-hidden="true"></i></i><i class="fa fa-database" aria-hidden="true"></i><i class="fa fa-wifi" aria-hidden="true"></i><i class="fa fa-bluetooth" aria-hidden="true"></i>'
+    for(let i =0; i<(largura/60);i++){
         var divNova = document.createElement("div")
         divNova.innerHTML = cont
         divAtual.appendChild(divNova)
