fc86f7dd1e8418f4c3a589534e10463d26a61b3b
Syri-Yagowo <yagovictorbotafogo@gmail.com>
2021-07-20 19:09:04 -0300

    estilizando .informacao | ajustando back-icons

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 window.onload = addElemento;
-var div=10
+var div=124000
 function addElemento(){
     var largura = document.body.clientHeight
     console.log(largura)
     var divAtual = document.getElementById('row')
     var cont = '<i class="fa fa-linux" aria-hidden="true"></i><i class="fa fa-windows" aria-hidden="true"></i><i class="fa fa-android" aria-hidden="true"></i><i class="fa fa-github" aria-hidden="true"></i><i class="fa fa-stack-overflow" aria-hidden="true"></i><i class="fa fa-code" aria-hidden="true"></i><i class="fa fa-terminal" aria-hidden="true"></i></i><i class="fa fa-database" aria-hidden="true"></i><i class="fa fa-wifi" aria-hidden="true"></i><i class="fa fa-bluetooth" aria-hidden="true"></i>'
     console.log(largura)
-    if(largura >= 1600){
+    if(largura > 1200){
         div = 0
     }
-    for(let i =0; i<largura/div; i++){
+    for(let i =1; i<=div/largura; i++){
         var divNova = document.createElement("span")
         divNova.innerHTML = cont
         divAtual.appendChild(divNova)
