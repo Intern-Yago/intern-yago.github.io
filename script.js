51cb2c107f7e231c21a2c7c48ce9bb60777ae422
Intern-Yago <yagovictorbotafogo@gmail.com>
2021-11-18 15:23:54 -0300

    add manifest

--- script.js
+++ script.js
@@ -2,10 +2,8 @@ window.onload = addElemento;
 var div=190000
 function addElemento(){
     var largura = document.body.clientHeight
-    console.log(largura)
     var divAtual = document.getElementById('row')
     var cont = '<i class="fa fa-linux" aria-hidden="true"></i><i class="fa fa-windows" aria-hidden="true"></i><i class="fa fa-android" aria-hidden="true"></i><i class="fa fa-github" aria-hidden="true"></i><i class="fa fa-stack-overflow" aria-hidden="true"></i><i class="fa fa-code" aria-hidden="true"></i><i class="fa fa-terminal" aria-hidden="true"></i></i><i class="fa fa-database" aria-hidden="true"></i><i class="fa fa-wifi" aria-hidden="true"></i><i class="fa fa-bluetooth" aria-hidden="true"></i>'
-    console.log(largura)
     if(largura > 1700){
         div = 0
     }
